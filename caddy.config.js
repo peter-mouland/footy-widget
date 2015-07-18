dae47801d16ee3e67effe641f4b42b39cceaa920
Peter Mouland <peter.mouland@gmail.com>
2015-07-18 22:17:14 +0100

    update caddy

--- caddy.config.js
+++ caddy.config.js
@@ -3,15 +3,13 @@ var pkg = require('./package.json');
 module.exports = {
     pkg: pkg,
     buildPaths: [
-        {source: "./src", targets: ['./_footy-ext'], minify: true},
-        {source: "./test/fixtures", targets: ['./_footy-ext']}
+        {source: "./src", target: './_footy-ext', minify: true},
+        {source: "./test/fixtures", target: './_footy-ext'}
     ],
     tasks : {
-        copy: ['fonts', 'images', 'server-config', '**/*.json'],
+        copy: ['images', '/*{CNAME,.htaccess,robots.txt,manifest.json}', '**/*.json'],
         build: ['sass', 'mustache', 'browserify'],
         serve: 'staticApp',
-        test: 'karma',
         release: ['git', 'gh-pages']
-    },
-    karma: ['./test/karma.unit.js']
+    }
 };
